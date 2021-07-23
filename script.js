5ff86d501c85ca97ee907910ec4ff17781ab9205
Daniel Silva Rinaldi <danielsilvarinaldi@hotmail.com>
2021-07-22 22:54:32 -0300

    atualização e adições de codigos

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-const pesqCod = document.getElementById("codigoProduto")
+const pesqCod = document.getElementById('codigoProduto')
 const codigosProdutos = {
     123:{
         Descrição : 'nome do produto',
-        Tipo : 'tipo do produto',
         Preço: {vista: '50,00',
                 prazo: '60,00'},
         Marca: 'marca teste',
     },
-    124:{
-        Descrição : 'nome do produto 124',
-        Tipo : 'tipo do produto 124',
-        Preço: {vista: '50,00',
-                prazo: '60,00'},
-        Marca: 'marca teste 124',
+    2355:{
+        Descrição : 'PINGENTE FOLHEADO C/ BR. FOLHA',
+        Preço: {vista: '55,00',
+                prazo: '62,00'},
+        Marca: 'HERMANO JOIAS',
     },
+    19504:{
+        Descrição : 'PINGENTE FOLHEADO OVAL ACRILICO 1 BONECA',
+        Preço: {vista: '45,00',
+                prazo: '50,00'},
+        Marca: 'HERMANO JOIAS',
+    },
+    22954:{
+        Descrição : 'PINGENTE FOLHEADO C/ 1 ZIRCONIA CABEÇA CAVALO',
+        Preço: {vista: '61,00',
+                prazo: '68,00'},
+        Marca: 'HERMANO JOIAS',
+    },
+    27958:{
+        Descrição : 'PINGENTE FOLHEADO ORAÇÃO PAI NOSSO ',
+        Preço: {vista: '32,00',
+                prazo: '35,00'},
+        Marca: 'HERMANO JOIAS',
+    },
+    28396:{
+        Descrição : 'PINGENTE FOLHEADO P. LARANJA TR. CORAÇÃO 3 GARR.15',
+        Preço: {vista: '33,00',
+                prazo: '36,00'},
+        Marca: 'HERMANO JOIAS',
+    }                                                
 }
-
 function pesquisar(){
     const resultado = document.getElementById('res')
     const codProd = Number(pesqCod.value)
     if(codigosProdutos[codProd] == undefined){
         window.alert ('ERRO. Código digitado não é valido')
         pesqCod.focus() // para voltar o foco no input do código
+        resultado.innerHTML=''
         
     }else{
         resultado.innerHTML = `<strong>${codigosProdutos[codProd].Descrição}</strong><br>`
-        resultado.innerHTML += `<strong>${codigosProdutos[codProd].Tipo}</strong><br>` 
-        resultado.innerHTML += `<strong>à vista R$${codigosProdutos[codProd].Preço.vista} à prazo R$${codigosProdutos[codProd].Preço.prazo}</strong><br>`
+        resultado.innerHTML += `<strong>À VISTA R$ ${codigosProdutos[codProd].Preço.vista} À PRAZO R$ ${codigosProdutos[codProd].Preço.prazo}</strong><br>`
         resultado.innerHTML += `<strong>${codigosProdutos[codProd].Marca}</strong><br>`
         pesqCod.focus()
     }
